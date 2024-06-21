9bf780161b1c31b07e6b8a229cd3100dafab40b4
Preethi586 <preethisarvani123@gmail.com>
2024-06-21 09:19:30 +0300

    This is my second commit

--- src/test/java/com/inetbanking/testCases/TC_AddCust_003.java
+++ src/test/java/com/inetbanking/testCases/TC_AddCust_003.java
@@ -18,6 +18,7 @@ public class TC_AddCust_003 extends BaseClass {
 	public void addNewCustomer() throws InterruptedException, IOException {
      //New line 0621
 		//Add a code
+		//Add an other code
 		String testCaseNAme = "Add NeW Customer";
 		test = extent.createTest("Test Case 1", "PASSED test case");
 		driver.get(baseURL);
